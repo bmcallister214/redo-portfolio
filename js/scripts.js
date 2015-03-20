aac9d0b2bdd34442c79d4d48fcdcb3d022f2dc33
bmcallister214 <brynmcallister@Bryns-MacBook-Pro.local>
2015-03-20 11:13:59 -0700

    stuff

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,7 @@ $('.fotorama').fotorama({
   width: 900,
   transition: 'dissolve'
 });
+
+var t = new Trianglify();
+var pattern = t.generate(document.body.clientWidth, document.body.clientHeight);
+document.body.setAttribute('style', 'background-image: '+pattern.dataUrl);
